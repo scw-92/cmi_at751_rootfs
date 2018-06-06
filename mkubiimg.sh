3f55355348c8beb7e11dd19e083e92c50ca14bab
scw-92 <1142344150@qq.com>
2018-06-06 10:18:34 +0800

    change the size of ubifs to 480M

--- mkubiimg.sh
+++ mkubiimg.sh
@@ -2,7 +2,7 @@
 
 #sudo mkfs.ubifs  -q -r rootfs -m 2048 -e 126976 -c 1600 -o  ubifs.img -F
 #sudo mkfs.ubifs  -q -r rootfs -m 2048 -e 126976 -c 1600 -o  ubifs.img -F
-sudo mkfs.ubifs  -q -r rootfs -m 2048 -e 126976 -c 3840 -o  ubifs.img -F
+sudo mkfs.ubifs  -q -r rootfs -m 2048 -e 126976 -c 3600 -o  ubifs.img -F
 
 echo mkfs.ubifs over!
 sudo ubinize -o ubi.img -m 2048 -p 128KiB ubinize.cfg
